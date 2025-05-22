7cd690f639012321f11d92eae676fcb485ff6132
Luis Dale <luis.gcodes@gmail.com>
2025-05-22 16:34:42 -0400

    Exploring flakes
    Cleaning up my base system free of tools forgotten

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Personal Hugo website";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.hugo
+          ];
+
+          shellHook = ''
+            echo "Welcome to your Hugo dev shell!"
+          '';
+        };
+      });
+}
